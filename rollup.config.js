43c52b947896cb523ef16261504a55a0c3253808
Paul Savoldelli <p.savoldelli@criteo.com>
2018-09-05 16:43:55 +0200

    try with lodash internal function

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 
-import babel from 'rollup-plugin-babel';
+import babel   from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default {
     input: 'src/index.js',
@@ -7,7 +8,11 @@ export default {
         file: 'dist/index.module.js',
         format: 'es'
     },
+    external: [
+        'react'
+    ],
     plugins: [
+        resolve(),
         babel({
             exclude: 'node_modules/**'
         })
--- src/components/Text.js
+++ src/components/Text.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { reverse } from 'lodash-es';
 
 export class Text extends Component {
     render() {
         const display = 'text: ';
+        const props   = reverse(this.props.content);
+        
         return (
             <div>
                 { display + ' '  + this.props.content } 
