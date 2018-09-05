60927e7476239a00bf65183208f964f18b33ce62
Paul Savoldelli <p.savoldelli@criteo.com>
2018-09-05 12:29:17 +0200

    tree-shake component lib

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+
+const path    = require('path');
+
+module.exports = {
+    watch: true,
+    mode: 'production',
+    devtool: false,
+    optimization: {
+      sideEffects: false
+    },
+    entry: {
+        index: './src/index.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js',
+    },
+    externals : [ 'react', 'react-dom' ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: [
+                        [ "@babel/env", { "modules": false}],
+                        [ "@babel/react" ]
+                    ]
+                }
+            }
+        }]
+    }
+};
+  
